3874dd96a9521f8c5e382d3172981889f2549c2b
TewodrosAdimas <yosefenadagim@gmail.com>
2024-07-19 18:26:38 +0300

    practice

--- advanced_OOP/inheritance.py
+++ advanced_OOP/inheritance.py
@@ -1,19 +1,26 @@
 class Animal:
-    def __init__(self,type, ) :
-        pass
+    def __init__(self,type, country) :
+        self.type = type
+        self.country = country
 
-    def eat(self):
-        pass
+    def eat(self, food):
+        self.food = food
+        return f"{self.type} eats {food}"
 
     def sleep(self):
-        pass
+        return "sleep everyday"
 
 
 
 class Dog(Animal):
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(self, type, country, sound):
+        super().__init__(type, country)
+        self.sound = sound
 
     def bark(self):
-        pass
+        return f"bark as {self.sound}"
+
+dog1 = Animal("Mamal", "Ethiopia")
+print(dog1.eat("meat"))
+dog1 = Dog("Mamal", "Ethiopia", "Awwww")
+print(dog1.eat("meat"))
